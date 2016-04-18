2f8ffeec6a22272979680d75162508059f27b531
Lee Leahy <leroy.p.leahy@intel.com>
2016-04-18 09:33:33 -0700

    QuarkFspPkg/PlatformInit: Fix build error
    Fix build error detected with GCC 4.8
    Remove dead code
    
    Change-Id: I0eef72518b9133954ea46680897ab9c78354c286
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Lee Leahy <leroy.p.leahy@intel.com>

--- QuarkFspPkg/Platform/Pei/PlatformInit/BiosCodePreMrc.c
+++ QuarkFspPkg/Platform/Pei/PlatformInit/BiosCodePreMrc.c
@@ -21,26 +21,6 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 #include "CommonHeader.h"
 #include "PlatformEarlyInit.h"
-//#include "PeiFvSecurity.h"
-
-//EFI_STATUS
-//EFIAPI
-//EndOfPeiSignalPpiNotifyCallback (
-//  IN EFI_PEI_SERVICES           **PeiServices,
-//  IN EFI_PEI_NOTIFY_DESCRIPTOR  *NotifyDescriptor,
-//  IN VOID                       *Ppi
-//  );
-
-//
-// Function prototypes to routines implemented in other source modules
-// within this component.
-//
-
-//EFI_STATUS
-//EFIAPI
-//PlatformErratasPostMrc (
-//  VOID
-//  );
 
 //
 // The global indicator, the FvFileLoader callback will modify it to TRUE after loading PEIM into memory
@@ -52,43 +32,6 @@ UINTN                         mBoardLegacyGpioConfigTableLen = (sizeof(mBoardLeg
 BOARD_GPIO_CONTROLLER_CONFIG  mBoardGpioControllerConfigTable[]  = { PLATFORM_GPIO_CONTROLLER_CONFIG_DEFINITION };
 UINTN                         mBoardGpioControllerConfigTableLen = (sizeof(mBoardGpioControllerConfigTable) / sizeof(BOARD_GPIO_CONTROLLER_CONFIG));
 UINT8                         ChipsetDefaultMac [6] = {0xff,0xff,0xff,0xff,0xff,0xff};
-
-//EFI_PEI_PPI_DESCRIPTOR mPpiBootMode[1] = {
-//  {
-//    (EFI_PEI_PPI_DESCRIPTOR_PPI | EFI_PEI_PPI_DESCRIPTOR_TERMINATE_LIST),
-//    &gEfiPeiMasterBootModePpiGuid,
-//    NULL
-//  }
-//};
-
-//EFI_PEI_NOTIFY_DESCRIPTOR mMemoryDiscoveredNotifyList[1] = {
-//  {
-//    (EFI_PEI_PPI_DESCRIPTOR_NOTIFY_CALLBACK | EFI_PEI_PPI_DESCRIPTOR_TERMINATE_LIST),
-//    &gEfiPeiMemoryDiscoveredPpiGuid,
-//    MemoryDiscoveredPpiNotifyCallback
-//  }
-//};
-
-//EFI_PEI_NOTIFY_DESCRIPTOR mEndOfPeiSignalPpiNotifyList[1] = {
-//  {
-//    (EFI_PEI_PPI_DESCRIPTOR_NOTIFY_CALLBACK | EFI_PEI_PPI_DESCRIPTOR_TERMINATE_LIST),
-//    &gEfiEndOfPeiSignalPpiGuid,
-//    EndOfPeiSignalPpiNotifyCallback
-//  }
-//};
-
-//EFI_PEI_STALL_PPI mStallPpi = {
-//  PEI_STALL_RESOLUTION,
-//  Stall
-//};
-//
-//EFI_PEI_PPI_DESCRIPTOR mPpiStall[1] = {
-//  {
-//    (EFI_PEI_PPI_DESCRIPTOR_PPI | EFI_PEI_PPI_DESCRIPTOR_TERMINATE_LIST),
-//    &gEfiPeiStallPpiGuid,
-//    &mStallPpi
-//  }
-//};
 
 /**
   Set Mac address on chipset ethernet device.
@@ -193,212 +136,6 @@ SetLanControllerMacAddr (
 }
 
 /**
-  Initialize state of I2C GPIO expanders.
-
-  @param  PlatformType  Platform type for GPIO expander init.
-
-**/
-//EFI_STATUS
-//EarlyPlatformConfigGpioExpanders (
-//  IN CONST EFI_PLATFORM_TYPE              PlatformType
-//  )
-//{
-//  EFI_STATUS              Status;
-//  EFI_I2C_DEVICE_ADDRESS  I2CSlaveAddress;
-//  UINTN                   Length;
-//  UINTN                   ReadLength;
-//  UINT8                   Buffer[2];
-//
-//  //
-//  // Configure GPIO expanders for Galileo Gen 2
-//  // Route I2C pins to Arduino header
-//  // Set all GPIO expander pins connected to the Reset Button as inputs
-//  //
-//  if (PlatformType == GalileoGen2) {
-//    //
-//    // Configure AMUX1_IN (EXP2.P1_4) as an output
-//    //
-//    PlatformPcal9555GpioSetDir (
-//      GALILEO_GEN2_IOEXP2_7BIT_SLAVE_ADDR,  // IO Expander 2.
-//      12,                                   // P1-4.
-//      FALSE                                 // Configure as output
-//      );
-//
-//    //
-//    // Set AMUX1_IN(EXP2.P1_4) low to route I2C to Arduino Shield connector
-//    //
-//    PlatformPcal9555GpioSetLevel (
-//      GALILEO_GEN2_IOEXP2_7BIT_SLAVE_ADDR,  // IO Expander 2.
-//      12,                                   // P1-4. 
-//      FALSE                                 // Set pin low
-//      );
-//
-//    //
-//    // Configure Reset Button(EXP1.P1_7) as an input
-//    //
-//    PlatformPcal9555GpioSetDir (
-//      GALILEO_GEN2_IOEXP1_7BIT_SLAVE_ADDR,  // IO Expander 1.
-//      15,                                   // P1-7.
-//      TRUE
-//      );
-//
-//    //
-//    // Disable pullup on Reset Button(EXP1.P1_7)
-//    //
-//    PlatformPcal9555GpioDisablePull (
-//      GALILEO_GEN2_IOEXP1_7BIT_SLAVE_ADDR,  // IO Expander 1.
-//      15                                    // P1-7.
-//      );
-//
-//    //
-//    // Configure Reset Button(EXP2.P1_7) as an input
-//    //
-//    PlatformPcal9555GpioSetDir (
-//      GALILEO_GEN2_IOEXP2_7BIT_SLAVE_ADDR,  // IO Expander 2.
-//      15,                                   // P1-7.
-//      TRUE
-//      );
-//
-//    //
-//    // Disable pullup on Reset Button(EXP2.P1_7)
-//    //
-//    PlatformPcal9555GpioDisablePull (
-//      GALILEO_GEN2_IOEXP2_7BIT_SLAVE_ADDR,  // IO Expander 2.
-//      15                                    // P1-7.
-//      );
-//  }
-//
-//  //
-//  // Configure GPIO expanders for Galileo Gen 2
-//  // Set all GPIO expander pins connected to the Reset Button as inputs
-//  // Route I2C pins to Arduino header
-//  //
-//  if (PlatformType == Galileo) {
-//    //
-//    // Detect the I2C Slave Address of the GPIO Expander
-//    //
-//    if (PlatformLegacyGpioGetLevel (R_QNC_GPIO_RGLVL_RESUME_WELL, GALILEO_DETERMINE_IOEXP_SLA_RESUMEWELL_GPIO)) {
-//      I2CSlaveAddress.I2CDeviceAddress = GALILEO_IOEXP_J2HI_7BIT_SLAVE_ADDR;
-//    } else {
-//      I2CSlaveAddress.I2CDeviceAddress = GALILEO_IOEXP_J2LO_7BIT_SLAVE_ADDR;
-//    }
-//    DEBUG ((EFI_D_INFO, "Galileo GPIO Expender Slave Address = %02x\n", I2CSlaveAddress.I2CDeviceAddress));
-//
-//    //
-//    // Set I2C_MUX (GPORT1_BIT5) low to route I2C to Arduino Shield connector
-//    //
-//
-//    //
-//    // Select GPIO Expander GPORT1
-//    //
-//    Length = 2;
-//    Buffer[0] = 0x18; //sub-address
-//    Buffer[1] = 0x01; //data
-//    Status = I2cWriteMultipleByte (
-//      I2CSlaveAddress,
-//      EfiI2CSevenBitAddrMode,
-//      &Length,
-//      &Buffer
-//      );
-//    ASSERT_EFI_ERROR (Status);
-//
-//    //
-//    // Read "Pin Direction" of GPIO Expander GPORT1
-//    //
-//    Length = 1;
-//    ReadLength = 1;
-//    Buffer[1] = 0x1C;
-//    Status = I2cReadMultipleByte (
-//      I2CSlaveAddress,
-//      EfiI2CSevenBitAddrMode,
-//      &Length,
-//      &ReadLength,
-//      &Buffer[1]
-//      );
-//    ASSERT_EFI_ERROR (Status);
-//
-//    //
-//    // Configure GPIO Expander GPORT1_BIT5 as an output
-//    //
-//    Length = 2;
-//    Buffer[0] = 0x1C; //sub-address
-//    Buffer[1] = (UINT8)(Buffer[1] & (~BIT5)); //data
-//
-//    Status = I2cWriteMultipleByte (
-//      I2CSlaveAddress,
-//      EfiI2CSevenBitAddrMode,
-//      &Length,
-//      &Buffer
-//      );
-//    ASSERT_EFI_ERROR (Status);
-//
-//    //
-//    // Set GPIO Expander GPORT1_BIT5 low
-//    //
-//    Length = 2;
-//    Buffer[0] = 0x09; //sub-address
-//    Buffer[1] = (UINT8)(~BIT5); //data
-//
-//    Status = I2cWriteMultipleByte (
-//      I2CSlaveAddress,
-//      EfiI2CSevenBitAddrMode,
-//      &Length,
-//      &Buffer
-//      );
-//    ASSERT_EFI_ERROR (Status);
-//
-//    //
-//    // Configure RESET_N_SHLD (GPORT5_BIT0) and SW_RESET_N_SHLD (GPORT5_BIT1) as inputs
-//    //
-//
-//    //
-//    // Select GPIO Expander GPORT5
-//    //
-//    Length = 2;
-//    Buffer[0] = 0x18;
-//    Buffer[1] = 0x05;
-//    Status = I2cWriteMultipleByte (
-//      I2CSlaveAddress,
-//      EfiI2CSevenBitAddrMode,
-//      &Length,
-//      &Buffer
-//      );
-//    ASSERT_EFI_ERROR (Status);
-//
-//    //
-//    // Read "Pin Direction" of GPIO Expander GPORT5
-//    //
-//    Length = 1;
-//    ReadLength = 1;
-//    Buffer[1] = 0x1C;
-//    Status = I2cReadMultipleByte (
-//      I2CSlaveAddress,
-//      EfiI2CSevenBitAddrMode,
-//      &Length,
-//      &ReadLength,
-//      &Buffer[1]
-//      );
-//    ASSERT_EFI_ERROR (Status);
-//
-//    //
-//    // Configure GPIO Expander GPORT5_BIT0 and GPORT5_BIT1 as inputs
-//    //
-//    Length = 2;
-//    Buffer[0] = 0x1C;
-//    Buffer[1] = Buffer[1] | BIT0 | BIT1;
-//    Status = I2cWriteMultipleByte (
-//      I2CSlaveAddress,
-//      EfiI2CSevenBitAddrMode,
-//      &Length,
-//      &Buffer
-//      );
-//    ASSERT_EFI_ERROR (Status);
-//  }
-//
-//  return EFI_SUCCESS;
-//}
-
-/**
   This is the entrypoint of PEIM
 
   @param  FileHandle  Handle of the file being invoked.
@@ -415,21 +152,17 @@ BiosBeforeMemoryInit(
 {
   EFI_STATUS                              Status = EFI_SUCCESS;
   EFI_BOOT_MODE                           BootMode;
-  //EFI_PEI_STALL_PPI                       *StallPpi;
-  //EFI_PEI_PPI_DESCRIPTOR                  *StallPeiPpiDescriptor;
-  //EFI_FV_FILE_INFO                        FileInfo;
-  EFI_PLATFORM_TYPE                       PlatformType;
 
   //
-  // Workaround: 
+  // Workaround:
   //   This driver will be invoked before MemoryInitUpd pointer initailized.
   //   To avoid the system halt, just initialized it before MemoryInitUpd used.
   //
-  // Background: 
+  // Background:
   //   According to current architect, pointer of UPD data region is equal to
   //   pointer of Memory Init UPD data region.
   //
-  // Note: 
+  // Note:
   //   Since the whole BiosCodePreMrc and BiosCodePostMrc PEIM will be
   //   removed in the future, so it's not a problem.
   //
@@ -440,56 +173,11 @@ BiosBeforeMemoryInit(
 
   //PlatformType = (EFI_PLATFORM_TYPE)PcdGet16 (PcdPlatformType);
   MemoryInitUpdPtr = (MEMORY_INIT_UPD *)GetFspMemoryInitUpdDataPointer();
-  PlatformType = MemoryInitUpdPtr->PcdPlatformType;
-
-  //
-  // Initialize Firmware Volume security.
-  // This must be done before any firmware volume accesses (excl. BFV)
-  //
-  //Status = PeiInitializeFvSecurity();
-  //ASSERT_EFI_ERROR (Status);
 
   //
   // Do any early platform specific initialization.
   //
   EarlyPlatformInit ();
-
-  //
-  // This is a second path on entry, in recovery boot path the Stall PPI need to be memory-based
-  // to improve recovery performance.
-  //
-  //Status = PeiServicesFfsGetFileInfo (FileHandle, &FileInfo);
-  //ASSERT_EFI_ERROR (Status);
-  //
-  // The follow conditional check only works for memory-mapped FFS,
-  // so we ASSERT that the file is really a MM FFS.
-  //
-  //ASSERT (FileInfo.Buffer != NULL);
-  //if (!(((UINTN) FileInfo.Buffer <= (UINTN) PeiInitPlatform) &&
-  //      ((UINTN) PeiInitPlatform <= (UINTN) FileInfo.Buffer + FileInfo.BufferSize))) {
-    //
-    // Now that module in memory, update the
-    // PPI that describes the Stall to other modules
-    //
-  //  Status = PeiServicesLocatePpi (
-  //             &gEfiPeiStallPpiGuid,
-  //             0,
-  //             &StallPeiPpiDescriptor,
-  //             (VOID **) &StallPpi
-  //             );
-
-  //  if (!EFI_ERROR (Status)) {
-
-  //    Status = PeiServicesReInstallPpi (
-  //               StallPeiPpiDescriptor,
-  //               &mPpiStall[0]
-  //               );
-  //  } else {
-
-  //    Status = PeiServicesInstallPpi (&mPpiStall[0]);
-  //  }
-  //  return Status;
-  //}
 
   //
   // Initialize System Phys
@@ -510,84 +198,13 @@ BiosBeforeMemoryInit(
   // Signal possible dependent modules that there has been a
   // final boot mode determination
   //
-  //if (!EFI_ERROR(Status)) {
-  //  Status = PeiServicesInstallPpi (&mPpiBootMode[0]);
-  //  ASSERT_EFI_ERROR (Status);
-  //}
 
   if (BootMode != BOOT_ON_S3_RESUME) {
     QNCClearSmiAndWake ();
   }
 
-  //DEBUG ((EFI_D_INFO, "MRC Entry\n"));
-  //MemoryInit ((EFI_PEI_SERVICES**)PeiServices);
-
-  //
-  // Do Early PCIe init.
-  //
-  //DEBUG ((EFI_D_INFO, "Early PCIe controller initialization\n"));
-  //PlatformPciExpressEarlyInit (PlatformType);
-
-
-  //DEBUG ((EFI_D_INFO, "Platform Erratas After MRC\n"));
-  //PlatformErratasPostMrc ();
-
-  //
-  //
-  //
-  //DEBUG ((EFI_D_INFO, "EarlyPlatformConfigGpioExpanders ()\n"));
-  //EarlyPlatformConfigGpioExpanders (PlatformType);
-
-  //
-  // Now that all of the pre-permanent memory activities have
-  // been taken care of, post a call-back for the permanent-memory
-  // resident services, such as HOB construction.
-  // PEI Core will switch stack after this PEIM exit.  After that the MTRR
-  // can be set.
-  //
-  //Status = PeiServicesNotifyPpi (&mMemoryDiscoveredNotifyList[0]);
-  //ASSERT_EFI_ERROR (Status);
-/*
-
-  if (BootMode != BOOT_ON_S3_RESUME) {
-    Status = PeiServicesNotifyPpi (mEndOfPeiSignalPpiNotifyList);
-    ASSERT_EFI_ERROR (Status);
-  }
-*/
-  //if (BootMode == BOOT_IN_RECOVERY_MODE) {
-  //  PeiServicesRegisterForShadow (FileHandle);
-  //}
-
   return Status;
 }
-
-//EFI_STATUS
-//EFIAPI
-//EndOfPeiSignalPpiNotifyCallback (
-//  IN EFI_PEI_SERVICES           **PeiServices,
-//  IN EFI_PEI_NOTIFY_DESCRIPTOR  *NotifyDescriptor,
-//  IN VOID                       *Ppi
-//  )
-//{
-//  EFI_STATUS                            Status;
-//
-//  DEBUG ((EFI_D_INFO, "End of PEI Signal Callback\n"));
-//
-//    //
-//  // Restore the flash region to be UC
-//  // for both normal boot as we build a Resource Hob to
-//  // describe this region as UC to DXE core.
-//  //
-//  WriteBackInvalidateDataCacheRange (
-//    (VOID *) (UINTN) PcdGet32 (PcdFlashAreaBaseAddress),
-//    PcdGet32 (PcdFlashAreaSize)
-//  );
-//
-//  Status = MtrrSetMemoryAttribute (PcdGet32 (PcdFlashAreaBaseAddress), PcdGet32 (PcdFlashAreaSize), CacheUncacheable);
-//  ASSERT_EFI_ERROR (Status);
-//
-//  return EFI_SUCCESS;
-//}
 
 /**
   This function will initialize USB Phy registers associated with QuarkSouthCluster.
@@ -693,104 +310,6 @@ EarlyPlatformThermalSensorInit (
 }
 
 /**
-  Print early platform info messages includeing the Stage1 module that's
-  running, MFH item list and platform data item list.
-**/
-//VOID
-//EFIAPI
-//EarlyPlatformInfoMessages (
-//  VOID
-//  )
-//{
-//  DEBUG_CODE_BEGIN ();
-//  QUARK_EDKII_STAGE1_HEADER       *Edk2ImageHeader;
-//
-//  //
-//  // Find which 'Stage1' image we are running and print the details
-//  //
-//  Edk2ImageHeader = (QUARK_EDKII_STAGE1_HEADER *) PcdGet32 (PcdEsramStage1Base);
-//  DEBUG ((EFI_D_INFO, "\n************************************************************\n"));
-//
-//  switch ((UINT8)Edk2ImageHeader->ImageIndex & QUARK_STAGE1_IMAGE_TYPE_MASK) {
-//    case QUARK_STAGE1_BOOT_IMAGE_TYPE:
-//      DEBUG ((EFI_D_INFO, "****  Quark EDKII Stage 1 Boot Image %d                ****\n", ((UINT8)Edk2ImageHeader->ImageIndex & ~(QUARK_STAGE1_IMAGE_TYPE_MASK))));
-//      break;
-//
-//    case QUARK_STAGE1_RECOVERY_IMAGE_TYPE:
-//      DEBUG ((EFI_D_INFO, "****  Quark EDKII Stage 1 Recovery Image %d            ****\n", ((UINT8)Edk2ImageHeader->ImageIndex & ~(QUARK_STAGE1_IMAGE_TYPE_MASK))));
-//      break;
-//
-//    default:
-//      DEBUG ((EFI_D_INFO, "****  Quark EDKII Unknown Stage 1 Image !!!!           ****\n"));
-//      break;
-//  }
-//  DEBUG (
-//    (EFI_D_INFO,
-//    "****  Quark EDKII Stage 2 Image 0x%08X:0x%08X ****\n" ,
-//    (UINTN) PcdGet32 (PcdFlashFvMainBase),
-//    (UINTN) PcdGet32 (PcdFlashFvMainSize)
-//    ));
-//
-//  DEBUG (
-//    (EFI_D_INFO,
-//    "****  Quark EDKII Payload Image 0x%08X:0x%08X ****\n" ,
-//    (UINTN) PcdGet32 (PcdFlashFvPayloadBase),
-//    (UINTN) PcdGet32 (PcdFlashFvPayloadSize)
-//    ));
-//
-//  DEBUG ((EFI_D_INFO, "************************************************************\n\n"));
-//
-//  DEBUG_CODE_END ();
-//}
-
-/**
-  Check if system reset due to error condition.
-
-  @param  ClearErrorBits  If TRUE clear error flags and value bits.
-
-  @retval TRUE  if system reset due to error condition.
-  @retval FALSE if NO reset error conditions.
-**/
-//BOOLEAN
-//CheckForResetDueToErrors (
-//  IN BOOLEAN                              ClearErrorBits
-//  )
-//{
-//  UINT32                            RegValue;
-//  BOOLEAN                           ResetDueToError;
-//
-//  ResetDueToError = FALSE;
-//
-//  //
-//  // Check if RMU reset system due to access violations.
-//  // RMU updates a SOC Unit register before reseting the system.
-//  //
-//  RegValue = QNCAltPortRead (QUARK_SCSS_SOC_UNIT_SB_PORT_ID, QUARK_SCSS_SOC_UNIT_CFG_STICKY_RW);
-//  if ((RegValue & B_CFG_STICKY_RW_VIOLATION) != 0) {
-//    ResetDueToError = TRUE;
-//
-//    DEBUG (
-//      (EFI_D_ERROR,
-//      "\nReset due to access violation: %s %s %s %s\n",
-//      ((RegValue & B_CFG_STICKY_RW_IMR_VIOLATION) != 0) ? L"'IMR'" : L".",
-//      ((RegValue & B_CFG_STICKY_RW_DECC_VIOLATION) != 0) ? L"'DECC'" : L".",
-//      ((RegValue & B_CFG_STICKY_RW_SMM_VIOLATION) != 0) ? L"'SMM'" : L".",
-//      ((RegValue & B_CFG_STICKY_RW_HMB_VIOLATION) != 0) ? L"'HMB'" : L"."
-//      ));
-//
-//    //
-//    // Clear error bits.
-//    //
-//    if (ClearErrorBits) {
-//      RegValue &= ~(B_CFG_STICKY_RW_VIOLATION);
-//      QNCAltPortWrite (QUARK_SCSS_SOC_UNIT_SB_PORT_ID, QUARK_SCSS_SOC_UNIT_CFG_STICKY_RW, RegValue);
-//    }
-//  }
-//
-//  return ResetDueToError;
-//}
-
-/**
   This function provides early platform initialization.
 
   @param  PlatformInfo  Pointer to platform Info structure.
@@ -809,21 +328,6 @@ EarlyPlatformInit (
   PlatformType = MemoryInitUpdPtr->PcdPlatformType;
 
   DEBUG ((EFI_D_INFO, "EarlyPlatformInit for PlatType=0x%02x\n", (UINTN) PlatformType));
-
-  //
-  // Check if system reset due to error condition.
-  //
-  //if (CheckForResetDueToErrors (TRUE)) {
-  //  if(FeaturePcdGet (WaitIfResetDueToError)) {
-  //    DEBUG ((EFI_D_ERROR, "Press any key to continue.\n"));
-  //    PlatformDebugPortGetChar8 ();
-  //  }
-  //}
-
-  //
-  // Display platform info messages.
-  //
-  //EarlyPlatformInfoMessages ();
 
   //
   // Early Legacy Gpio Init.
@@ -1195,4 +699,3 @@ EarlyPlatformMacInit (
       ));
   }
 }
-
