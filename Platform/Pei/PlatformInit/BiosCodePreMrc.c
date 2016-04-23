d2818c288a2610b0417ababf736cb98277aefa36
Lee Leahy <leroy.p.leahy@intel.com>
2016-04-23 12:37:52 -0700

    QuarkFspPkg/PlatformInit: Move USB initialization into BIOS
    Move the USB initialization into the BIOS (coreboot).
    
    Change-Id: Ice5f3bcb54fbf4fcaa3c032e36a4eb212b0492f0
    Contributed-under: TianoCore Contribution Agreement 1.0
    Signed-off-by: Lee Leahy <leroy.p.leahy@intel.com>

--- Platform/Pei/PlatformInit/BiosCodePreMrc.c
+++ Platform/Pei/PlatformInit/BiosCodePreMrc.c
@@ -21,90 +21,6 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 #include "CommonHeader.h"
 
-//
-// USB Phy Registers
-//
-#define USB2_GLOBAL_PORT  0x4001
-#define USB2_PLL1         0x7F02
-#define USB2_PLL2         0x7F03
-#define USB2_COMPBG       0x7F04
-
-/**
-  This function will initialize USB Phy registers associated with QuarkSouthCluster.
-
-  @param  VOID                  No Argument
-
-  @retval EFI_SUCCESS           All registers have been initialized
-**/
-VOID
-EFIAPI
-InitializeUSBPhy (
-    VOID
-   )
-{
-    UINT32 RegData32;
-
-    /** In order to configure the PHY to use clk120 (ickusbcoreclk) as PLL reference clock
-     *  and Port2 as a USB device port, the following sequence must be followed
-     *
-     **/
-
-    // Sideband register write to USB AFE (Phy)
-    RegData32 = QNCAltPortRead (QUARK_SC_USB_AFE_SB_PORT_ID, USB2_GLOBAL_PORT);
-    RegData32 &= ~(BIT1);
-    //
-    // Sighting #4930631 PDNRESCFG [8:7] of USB2_GLOBAL_PORT = 11b.
-    // For port 0 & 1 as host and port 2 as device.
-    //
-    RegData32 |= (BIT8 | BIT7);
-    QNCAltPortWrite (QUARK_SC_USB_AFE_SB_PORT_ID, USB2_GLOBAL_PORT, RegData32);
-
-    //
-    // Sighting #4930653 Required BIOS change on Disconnect vref to change to 600mV.
-    //
-    RegData32 = QNCAltPortRead (QUARK_SC_USB_AFE_SB_PORT_ID, USB2_COMPBG);
-    RegData32 &= ~(BIT10 | BIT9 | BIT8 | BIT7);
-    RegData32 |= (BIT10 | BIT7);
-    QNCAltPortWrite (QUARK_SC_USB_AFE_SB_PORT_ID, USB2_COMPBG, RegData32);
-
-    // Sideband register write to USB AFE (Phy)
-    // (pllbypass) to bypass/Disable PLL before switch
-    RegData32 = QNCAltPortRead (QUARK_SC_USB_AFE_SB_PORT_ID, USB2_PLL2);
-    RegData32 |= BIT29;
-    QNCAltPortWrite (QUARK_SC_USB_AFE_SB_PORT_ID, USB2_PLL2, RegData32);
-
-    // Sideband register write to USB AFE (Phy)
-    // (coreclksel) to select 120MHz (ickusbcoreclk) clk source.
-    // (Default 0 to select 96MHz (ickusbclk96_npad/ppad))
-    RegData32 = QNCAltPortRead (QUARK_SC_USB_AFE_SB_PORT_ID, USB2_PLL1);
-    RegData32 |= BIT1;
-    QNCAltPortWrite (QUARK_SC_USB_AFE_SB_PORT_ID, USB2_PLL1, RegData32);
-
-    // Sideband register write to USB AFE (Phy)
-    // (divide by 8) to achieve internal 480MHz clock
-    // for 120MHz input refclk.  (Default: 4'b1000 (divide by 10) for 96MHz)
-    RegData32 = QNCAltPortRead (QUARK_SC_USB_AFE_SB_PORT_ID, USB2_PLL1);
-    RegData32 &= ~(BIT5 | BIT4 | BIT3);
-    RegData32 |= BIT6;
-    QNCAltPortWrite (QUARK_SC_USB_AFE_SB_PORT_ID, USB2_PLL1, RegData32);
-
-    // Sideband register write to USB AFE (Phy)
-    // Clear (pllbypass)
-    RegData32 = QNCAltPortRead (QUARK_SC_USB_AFE_SB_PORT_ID, USB2_PLL2);
-    RegData32 &= ~BIT29;
-    QNCAltPortWrite (QUARK_SC_USB_AFE_SB_PORT_ID, USB2_PLL2, RegData32);
-
-    // Sideband register write to USB AFE (Phy)
-    // Set (startlock) to force the PLL FSM to restart the lock
-    // sequence due to input clock/freq switch.
-    RegData32 = QNCAltPortRead (QUARK_SC_USB_AFE_SB_PORT_ID, USB2_PLL2);
-    RegData32 |= BIT24;
-    QNCAltPortWrite (QUARK_SC_USB_AFE_SB_PORT_ID, USB2_PLL2, RegData32);
-
-    // At this point the PLL FSM and COMP FSM will complete
-
-}
-
 /**
   This function provides early platform Thermal sensor initialisation.
 **/
@@ -154,9 +70,6 @@ BiosBeforeMemoryInit(
   // Early Thermal Sensor Init.
   //
   EarlyPlatformThermalSensorInit ();
-
-  // Program USB Phy
-  InitializeUSBPhy();
 
   //
   // Do platform specific logic to create a boot mode
